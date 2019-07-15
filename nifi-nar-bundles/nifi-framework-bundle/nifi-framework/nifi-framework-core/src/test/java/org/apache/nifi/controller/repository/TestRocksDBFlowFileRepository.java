cae8486a3b44196b6845603a4131f145a25bad97
Brandon Devries <devriesb@apache.org>
2019-07-15 16:24:42 -0400

    testing for intermittent failure

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/test/java/org/apache/nifi/controller/repository/TestRocksDBFlowFileRepository.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-framework-core/src/test/java/org/apache/nifi/controller/repository/TestRocksDBFlowFileRepository.java
@@ -678,14 +678,18 @@ public class TestRocksDBFlowFileRepository {
     }
 
     private List<RepositoryRecord> getRecordsToDelete(List<RepositoryRecord> createdRecords, List<FlowFileRecord> flowFileCollection) {
-        assertNotNull(createdRecords);
-        assertNotNull(flowFileCollection);
         final Collection<Long> inMemoryIds = new ArrayList<>();
         for (FlowFileRecord flowFileRecord : flowFileCollection) {
             Long id = flowFileRecord.getId();
             inMemoryIds.add(id);
         }
-        return createdRecords.stream().filter(rec -> inMemoryIds.contains(rec.getCurrent().getId())).collect(Collectors.toList());
+        List<RepositoryRecord> list = new ArrayList<>();
+        for (RepositoryRecord rec : createdRecords) {
+            if (inMemoryIds.contains(rec.getCurrent().getId())) {
+                list.add(rec);
+            }
+        }
+        return list;
     }
 
     private Connection addConnectionToProvider(TestQueueProvider
